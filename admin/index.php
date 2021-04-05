536924f669646ac8f40aa60656afadfccaee33e1
Ze Feng Wang <zefengwang613@gmail.com>
2021-04-05 13:55:51 -0400

    Added User Online Feature

--- admin/index.php
+++ admin/index.php
@@ -2,7 +2,26 @@
 <?php include "includes/admin_header.php";?>
 <body>
     <div id="wrapper">
+    <?php
+        $session = session_id(); //catches all session tabs
+        $time = time();
+        $time_out_in_seconds = 60;
+        $time_out = $time - $time_out_in_seconds;
 
+        $query = "SELECT * FROM users_online WHERE session = '$session' ";
+        $send_query = mysqli_query($connection, $query);
+        $count = mysqli_num_rows($send_query);
+
+        if($count == NULL){
+            mysqli_query($connection, "INSERT INTO users_online(session, time) VALUES('$session', '$time')");
+        }else{
+            mysqli_query($connection, "UPDATE users_online SET time = '$time' WHERE session = '$session'");
+        }
+        $users_online_query = mysqli_query($connection, "SELECT * FROM users_online WHERE time > '$time_out'");
+        $count_user = mysqli_num_rows($users_online_query);
+
+
+    ?>
 
         <!-- Navigation -->
         <?php include "includes/admin_navigation.php";?>
@@ -17,6 +36,9 @@
                         <h1 class="page-header">
                             Welcome to Admin
                             <small><?php echo $_SESSION['username'];?></small>
+                        </h1>
+                        <h1>
+                            <?php echo $count_user;?>
                         </h1>
                     </div>
                 </div>

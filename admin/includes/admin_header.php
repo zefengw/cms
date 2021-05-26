eea5abe3cfaee8f3b5449200be64c1eb33cf1125
Ze Feng Wang <zefengwang613@gmail.com>
2021-05-26 19:32:36 -0400

    Fixed jquery errors

--- admin/includes/admin_header.php
+++ admin/includes/admin_header.php
@@ -43,7 +43,10 @@
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
     <link href="css/styles.css" rel="stylesheet">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tinymce/4.5.6/tinymce.min.js"></script>
     <script type="text/javascript" src="js/jquery.js"></script>
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/tinymce/4.5.6/jquery.tinymce.min.js"></script>
+    <script type="text/javascript" src="js/scripts.js"></script>
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
     <script type="text/javascript" src="https://cdn.ckeditor.com/ckeditor5/26.0.0/classic/ckeditor.js"></script>
     </head>

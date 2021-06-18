133af2dce92d2ffd61886a9387872cf5361e3848
Zefeng Wang <55709954+zefengw@users.noreply.github.com>
2021-06-18 13:57:08 -0400

    Delete Config.php

--- classes/Config.php
+++
@@ -1,9 +0,0 @@
-<?php
-    class Config {
-        //define() is more flexible than const
-        const SMTP_HOST = 'smtp.gmail.com';
-        const SMTP_PORT = 2525;
-        const SMTP_USER = '46d217a7438f11';
-        const SMTP_PASS = '9287127a901f34';
-    }
-?>
